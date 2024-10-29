1d622ab6925908366287fb13f83cf4718d73d813
LENOVO <sahrulcc07@gmail.com>
2024-10-29 21:19:06 +0700

    Commit ThreadHandler

--- app/src/main/java/polbeng/sahrulgunawan/threadrunnable/MainActivity.kt
+++ app/src/main/java/polbeng/sahrulgunawan/threadrunnable/MainActivity.kt
@@ -1,27 +1,37 @@
 package polbeng.sahrulgunawan.threadrunnable
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
 import polbeng.sahrulgunawan.threadrunnable.databinding.ActivityMainBinding
-
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var mHandler: Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        mHandler = MyHandler()
         binding.button.setOnClickListener {
             Thread {
                 killSomeTime()
             }.start()
         }
     }
+    @SuppressLint("HandlerLeak")
+    inner class MyHandler : Handler() {
+        override fun handleMessage(msg: Message) {
+            binding.textView.text = msg.data?.getString("counter")
+        }
+    }
     private fun killSomeTime() {
         for (i in 1..20) {
-            runOnUiThread {
-                binding.textView.text = i.toString()
-            }
+            val msg = Message.obtain()
+            msg.data.putString("counter", i.toString())
+            mHandler.sendMessage(msg)
             Thread.sleep(2000)
             println("i: $i")
         }
